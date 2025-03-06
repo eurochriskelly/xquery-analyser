76f88deb4cef6251eab068a3decb6053b762ebd7
Chris Kelly <chris.kelly@koop.overheid.nl>
2025-03-06 06:10:17 +0100

    Now allows gernation of functions in main modules

--- test/test.sh
+++ test/test.sh
@@ -3,3 +3,6 @@
 cd "$(git rev-parse --show-toplevel)"
 
 node src/extract-functions.js --file-name=test/data/main-fn1.xqy --out-dir=/tmp/test
+echo "press ENTER to view "
+read x
+less /tmp/test/test_data_main-fn2.xqy.json
